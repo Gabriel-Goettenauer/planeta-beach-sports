71a0fdcf0f7649f62d4d00e0ed6482eef8c0b9ed
Gabriel Goettenauer <gabrielgoettenauer@gmail.com>
2024-11-25 12:35:54 -0300

    alteração

--- .angular/cache/17.3.11/vite/deps/@angular_core.js
+++ .angular/cache/17.3.11/vite/deps/@angular_core.js
@@ -451,7 +451,7 @@ import {
   ɵɵvalidateIframeAttribute,
   ɵɵviewQuery,
   ɵɵviewQuerySignal
-} from "./chunk-5FUJWLSI.js";
+} from "./chunk-4MBVS4SB.js";
 export {
   ANIMATION_MODULE_TYPE,
   APP_BOOTSTRAP_LISTENER,
