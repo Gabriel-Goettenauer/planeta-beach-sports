3f79678b367b2dc4699160dcc976c1368fa64cb0
Gabriel Goettenauer <gabrielgoettenauer@gmail.com>
2024-11-27 12:31:51 -0300

    mudando a raka3 para raka4

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -16,7 +16,7 @@ export class ProductDetailsComponent implements OnInit {
   produtos = [
     { id: 1, nome: 'Raquete Beach Tennis BAZOOKA 6.0', preco: 2559.0, imagem: '/assets/raka1.svg' },
     { id: 2, nome: 'Raquete Beach Tennis PRO 8.0', preco: 2759.0, imagem: '/assets/raka2.svg' },
-    { id: 3, nome: 'Raquete Beach Tennis ELITE 10.0', preco: 2959.0, imagem: '/assets/raka3.svg' },
+    { id: 3, nome: 'Raquete Beach Tennis ELITE 10.0', preco: 2959.0, imagem: '/assets/raka4.svg' },
   ];
 
   constructor(private route: ActivatedRoute) {}
